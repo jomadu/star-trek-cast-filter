bc2eb477399dd8a6dbb37f2e51e507f2fc372b96
Max Dunn <maxdunn123@gmail.com>
2021-06-07 23:04:44 -0700

    added actual details to character detail component

--- src/CharacterResultDetail/CharacterResultDetail.js
+++ src/CharacterResultDetail/CharacterResultDetail.js
@@ -9,20 +9,86 @@ const CharacterResultDetail = ({ uid }) => {
     setUid(uid);
   }, [uid, setUid]);
 
-  var text;
+  var comp;
   if (error) {
-    text = "Error";
+    comp = <p>Error</p>;
   } else if (isLoading) {
-    text = "Loading...";
-  } else if (data) {
-    text = `Name: ${data.character.name}, Serial Number: ${data.character.serialNumber}`;
+    comp = <p>Loading...</p>;
+  } else if (data?.character) {
+    var c = data.character;
+
+    var series = new Set();
+
+    c.episodes.forEach((e) => {
+      series.add(e.series.title);
+    });
+    console.log(series);
+
+    comp = (
+      <table>
+        <thead>
+          <tr>
+            <th>Attribute</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>{c.name}</td>
+          </tr>
+          <tr>
+            <td>Titles</td>
+            <td>
+              {c.titles.length ? (
+                <ul>
+                  {c.titles.map((title) => (
+                    <li key={title.uid}>{title.name}</li>
+                  ))}
+                </ul>
+              ) : (
+                "None"
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td>Organizations</td>
+            <td>
+              {c.organizations.length ? (
+                <ul>
+                  {c.organizations.map((org) => (
+                    <li key={org.uid}>{org.name}</li>
+                  ))}
+                </ul>
+              ) : (
+                "None"
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td>Series</td>
+            <td>
+              {series.size ? (
+                <ul>
+                  {[...series].map((series, i) => (
+                    <li key={i}>{series}</li>
+                  ))}
+                </ul>
+              ) : (
+                "None"
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td># of Episodes</td>
+            <td>{c.episodes.length}</td>
+          </tr>
+        </tbody>
+      </table>
+    );
   }
 
-  return (
-    <div>
-      <p>{text}</p>
-    </div>
-  );
+  return <div>{comp}</div>;
 };
 
 CharacterResultDetail.propTypes = {
